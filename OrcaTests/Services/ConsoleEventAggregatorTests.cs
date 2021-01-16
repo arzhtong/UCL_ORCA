4b309276fa8c71e3833ffb3508d9f644945d131e
Omar Beyhum <omar.beyhum@gmail.com>
2021-01-16 12:40:22 +0000

    Add test project and basic conventions

---
+++ OrcaTests/Services/ConsoleEventAggregatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Orca.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orca.Services
+{
+    public class ConsoleEventAggregatorTests
+    {
+        [Fact]
+        public void DoNothingTest()
+        {
+            Assert.True(true, "This test needs an implementation");
+        }
+
+    }
+}
